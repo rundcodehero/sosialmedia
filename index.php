3ab663e2848ae8d77b757cc4efb9db5f127d6623
rund <rundcodehero@gmail.com>
2022-02-01 18:45:23 +0700

    new file add full

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DEVOPS CIRCLE</title>
+
+     <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">i
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Selamat datang di DEVOPS CIRCLE </h1>
+			<p>Bergabunglah bersama jutaan devops indonesia </p>
+			<p><h3 style="color:blue;"> "Does not matter how slowly you go so long as you..</h3></p>
+			<p><h2 style="color:blue;"> DO NOT STOP”</h2></p>  
+                    </div>
+                    <div class="col-md-4">
+                        <a href="login.php" class="btn btn-secondary">Masuk</a>
+                        <a href="register.php" class="btn btn-success">Daftar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img class="img img-responsive" src="img/connect.png" />
+                    </div>
+                </div>
+            </div>
+    </section>
+
+</body>
+</html>
